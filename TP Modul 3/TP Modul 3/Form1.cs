ccb2790327aafdcbea2bccef6696d8ba88216c20
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-03-01 16:47:01 +0700

    Menambahkan .vs ke .gitignore

--- TP Modul 3/TP Modul 3/Form1.cs
+++ TP Modul 3/TP Modul 3/Form1.cs
@@ -16,5 +16,16 @@ namespace TP_Modul_3
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text; 
+            label1.Text = "Halo " + inputText; 
+        }
     }
 }
